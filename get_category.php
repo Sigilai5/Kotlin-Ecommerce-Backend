0fd689a048f06cae742ebed8afe269e4d83ac7af
sigilai <briansigilai@gmail.com>
2018-11-21 23:13:03 +0300

    created get_items.php

--- get_category.php
+++ get_category.php
@@ -5,3 +5,17 @@
  * Date: 11/21/18
  * Time: 7:47 PM
  */
+
+$con=new mysqli("localhost","root","","ecommerce");
+$st_check=$con->prepare("SELECT DISTINCT category FROM items");  //DISTINCT HELPS US TO GET UNIQUE CATEGORIES ONLY
+$st_check->execute();
+$rs=$st_check->get_result();
+$arr=array();
+while ($row=$rs->fetch_assoc())
+{
+    array_push($arr,$row);
+}
+
+echo json_encode($arr)
+
+?>
---
+++ get_items.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sigilai
+ * Date: 11/21/18
+ * Time: 11:12 PM
+ */
