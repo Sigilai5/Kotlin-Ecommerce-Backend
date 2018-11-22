6224a50b3d140d94283f3737a753ec3a2fb2bce1
sigilai <briansigilai@gmail.com>
2018-11-22 14:18:12 +0300

    created add_temp.php

---
+++ add_temp.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sigilai
+ * Date: 11/22/18
+ * Time: 2:17 PM
+ */
--- get_items.php
+++ get_items.php
@@ -5,3 +5,22 @@
  * Date: 11/21/18
  * Time: 11:12 PM
  */
+
+
+$con=new mysqli("localhost","root","","ecommerce");
+$st_check=$con->prepare("SELECT * FROM items WHERE category=?");  //DISTINCT HELPS US TO GET UNIQUE CATEGORIES ONLY
+$st_check->bind_param("s",$_GET["category"]);
+$st_check->execute();
+$rs=$st_check->get_result();
+$arr=array();
+while ($row=$rs->fetch_assoc())
+{
+    array_push($arr,$row);
+}
+
+echo json_encode($arr)
+
+
+
+
+?>
