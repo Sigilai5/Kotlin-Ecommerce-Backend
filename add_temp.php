76eae97501bc78f2fcd27b14297199d9f23b9d1d
sigilai <briansigilai@gmail.com>
2018-11-22 14:34:53 +0300

    created get_temp.php

--- add_temp.php
+++ add_temp.php
@@ -5,3 +5,10 @@
  * Date: 11/22/18
  * Time: 2:17 PM
  */
+
+$con=new mysqli("localhost","root","","ecommerce");
+$st_check=$con->prepare("INSERT INTO temp_order VALUES (?,?,?)");
+$st_check->bind_param("sii",$_GET["mobile"],$_GET["item_id"],$_GET["quantity"]);
+$st_check->execute();
+
+?>
---
+++ get_temp.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sigilai
+ * Date: 11/22/18
+ * Time: 2:34 PM
+ */
+
+
+?>
