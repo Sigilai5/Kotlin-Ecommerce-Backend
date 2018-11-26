1229405aa7175a0077cebcec2437bc47396a2095
sigilai <briansigilai@gmail.com>
2018-11-26 18:46:49 +0300

    created cancel_temp

---
+++ cancel_temp.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sigilai
+ * Date: 11/26/18
+ * Time: 6:46 PM
+ */
--- get_temp.php
+++ get_temp.php
@@ -6,5 +6,18 @@
  * Time: 2:34 PM
  */
 
+$con=new mysqli("localhost","root","","ecommerce");
+$st_check=$con->prepare("SELECT id,name,price,quantity,mobile FROM temp_order INNER JOIN items on temp_order.item_id=items.id WHERE mobile=?");
+$st_check->bind_param("s",$_GET["mobile"]);
+$st_check->execute();
+$rs=$st_check->get_result();
+$arr=array();
+while ($row=$rs->fetch_assoc())
+{
+    array_push($arr,$row);
+}
+
+echo json_encode($arr)
+
 
 ?>
