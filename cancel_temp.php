8a4b3b4822f5e23dc6083cf44213a45d11b3f91b
sigilai <briansigilai@gmail.com>
2018-11-27 12:17:50 +0300

    created confirm order file

--- cancel_temp.php
+++ cancel_temp.php
@@ -5,3 +5,10 @@
  * Date: 11/26/18
  * Time: 6:46 PM
  */
+
+$con=new mysqli("localhost","root","","ecommerce");
+$st_check=$con->prepare("DELETE FROM temp_order WHERE mobile=?");
+$st_check->bind_param("s",$_GET["mobile"]);
+$st_check->execute();
+
+?>
---
+++ confirm_order.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sigilai
+ * Date: 11/27/18
+ * Time: 12:17 PM
+ */
