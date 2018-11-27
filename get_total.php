28be4a59d271ff6df5ecccf749c6c61f7fb6a126
sigilai <briansigilai@gmail.com>
2018-11-27 18:15:17 +0300

    created connfirm.php

--- get_total.php
+++ get_total.php
@@ -5,3 +5,18 @@
  * Date: 11/27/18
  * Time: 1:05 PM
  */
+
+$con=new mysqli("localhost","root","","ecommerce");
+$st=$con->prepare("SELECT price,quantity FROM items INNER JOIN bill_details ON items.id=bill_details.item_id WHERE bill_details.bill_no=?");
+$st->bind_param("i",$_GET["bill_no"]);
+$st->execute();
+$rs=$st->get_result();
+$total=0;
+while ($row=$rs->fetch_assoc())
+{
+    $total=$total + ($row["price"]*$row["quantity"]);
+}
+
+echo $total
+
+?>
